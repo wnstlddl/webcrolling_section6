05882095ca28c0c7a30744ff8bfb77441b65c126
wnstlddl <forawhile1104@gmail.com>
2020-01-27 21:37:06 +0900

    Setting youtube play,stop,set download path, download, progress bar

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import QtCore
 from PyQt5 import uic
-from PyQt5.QtCore import  pyqtSlot, pyqtSignal
+from PyQt5.QtCore import  pyqtSlot, pyqtSignal, QUrl
 from lib.YouViewerLayout import Ui_MainWindow
 from lib.AuthDialog import AuthDialog
 import re
 import datetime
+import pytube
 
 # form_class = uic.loadUiType("C:/python_Webcroling/section6/ui/you_viewer_v1.0.ui")[0]
 #cmd 창에 해당 루트 가서 이렇게 입력 pyuic5 -x you_viewer_v1.0.ui -o you_viewer_layout.py
@@ -22,6 +23,12 @@ class Main(QMainWindow, Ui_MainWindow):
         self.user_id = None
         self.user_pw = None
 
+        #재생여부
+        self.is_play = False
+
+        #유튜브 관련 작업
+        self.youtb = None
+        self.yoytbFileSize = 0
 
     # 기본 UI 비활성화
     def initAuthLock(self):
@@ -47,8 +54,17 @@ class Main(QMainWindow, Ui_MainWindow):
     def showStatusMsg(self,msg):
         self.statusbar.showMessage(msg)
 
+
+        #시그널 초기화
     def initSignal(self):
         self.logineButton.clicked.connect(self.authCheck)
+        self.previewButton.clicked.connect(self.load_url)
+        self.exitButton.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        self.webEngineView.loadProgress.connect(self.showProgressBrowserLoading)
+        self.fileNavButton.clicked.connect(self.selectDownPath)
+        self.calendarWidget.clicked.connect(self.append_data)
+        self.startButtom.clicked.connect(self.downloadYoutb)
+
 
     @pyqtSlot()
     def authCheck(self):
@@ -65,6 +81,54 @@ class Main(QMainWindow, Ui_MainWindow):
         else:
             QMessageBox.about(self,"인증오류","ID 또는 PW 오류")
 
+
+    def load_url(self):
+        url = self.urlTextEdit.text().strip() # strip 는 공백 없애기
+        v = re.compile('^https://www.youtube.com/?')
+        if self.is_play:
+            self.append_log_msg('Stop Click')
+            self.webEngineView.load(QUrl('about:blank'))
+            self.previewButton.setText("재생")
+            self.is_play = True
+            self.urlTextEdit.clear()
+            self.urlTextEdit.setFocus(True)
+            self.startButtom.setEnabled(False)
+            self.streamComboBox.clear()
+            self.progressBar_2.setValue(0)
+            self.showStatusMsg("인증완료")
+
+        else:
+            if v.match(url) is not None:
+                self.append_log_msg('Play Click')
+                self.webEngineView.load(QUrl(url))
+                self.showStatusMsg(url + "  재생 중")
+                self.previewButton.setText("중지")
+                self.is_play = True
+                self.startButtom.setEnabled(True)
+                self.initialYouWork(url)
+
+            else:
+                QMessageBox.about(self,"URL 형식오류","Youtube 주소 형식이 아닙니다.")
+                self.urlTextEdit.clear()
+                self.urlTextEdit.setFocus(True)
+
+
+    def initialYouWork(self,url):
+        video_list = pytube.YouTube(url)
+        #로딩바 계산
+        video_list.register_on_progress_callback(self.showProgressDownLoading)
+        self.youtb = video_list.streams.all()
+        self.streamComboBox.clear()
+        for q in self.youtb:
+            tmp_list, str_list = [], []
+            tmp_list.append(str(q.mime_type or ''))
+            tmp_list.append(str(q.res or ''))
+            tmp_list.append(str(q.fps or ''))
+            tmp_list.append(str(q.abr or ''))
+
+            str_list = [x for x in tmp_list if x !='']
+            self.streamComboBox.addItem(','.join(str_list))
+
     def append_log_msg(self,act):
         now = datetime.datetime.now()
         nowDatetime = now.strftime("%Y-%m-%d %H:%M:%S")
@@ -73,10 +137,48 @@ class Main(QMainWindow, Ui_MainWindow):
         self.plainTextEdit.appendPlainText(app_msg)
 
         #로그 저장
-        with open("C:/python/webcrolling/section6/webcrolling_section6/log/log.txt",'a') as f:
+        #mac
+        # with open("C:/python/webcrolling/section6/webcrolling_section6/log/log.txt",'a') as f:
+        #     f.write(app_msg + '\n')
+        #Desktop
+        with open("C:/python_Webcroling/section6/log/log.txt",'a') as f:
             f.write(app_msg + '\n')
 
+    @pyqtSlot(int)
+    def showProgressBrowserLoading(self, v):
+        self.progressBar.setValue(v)
 
+    @pyqtSlot()
+    def selectDownPath(self):
+        #파일선택
+        # fname = QFileDialog.getOpenFileName(self)
+        # self.pathTextEdit.setText(fname[0])
+
+        #경로선택
+        fpath = QFileDialog.getExistingDirectory(self,'Select Directory')
+        self.pathTextEdit.setText(fpath)
+
+    @pyqtSlot()
+    def append_data(self):
+        cur_date = self.calendarWidget.selectedDate()
+        log_append_day_msg = str(cur_date.year())+'-'+str(cur_date.month())+'-'+str(cur_date.day())
+        self.append_log_msg('Calender Click ')
+        self.append_log_msg(log_append_day_msg)
+
+    @pyqtSlot()
+    def downloadYoutb(self):
+        down_dir = self.pathTextEdit.text().strip()
+        if down_dir is None or down_dir =="" or not down_dir:
+            QMessageBox.about(self,'경로 선택','다운로드 받을 경로를 선택 하세요')
+            return None
+
+        self.yoytbFileSize = self.youtb[self.streamComboBox.currentIndex()].filesize
+        # print(self.yoytbFileSize)
+        self.youtb[self.streamComboBox.currentIndex()].download(down_dir)
+        self.append_log_msg('Download Click')
+
+    def showProgressDownLoading(self,stream,chunk,file_handle,bytes_remaining):
+        self.progressBar_2.setValue(int(((self.yoytbFileSize-bytes_remaining)/self.yoytbFileSize)*100))
 
 if __name__=="__main__":
     app = QApplication(sys.argv)
